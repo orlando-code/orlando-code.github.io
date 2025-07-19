602226d547c000bb6d2110f4128575ef06d058ed
Orlando Timmerman <rt582@cam.ac.uk>
2025-07-19 13:15:57 +0100

    re-added deleted X: thought it was referring to ex-twitter

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Github, Linkedin, Menu, Twitter } from 'lucide-react'
+import { Github, Linkedin, Menu, Twitter, X } from 'lucide-react'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { useState } from 'react'
