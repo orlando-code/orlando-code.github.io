2cbf8825713bc0a364c51b1ea301ae7b480e0705
Orlando Timmerman <rt582@cam.ac.uk>
2025-07-21 11:17:07 +0100

    updating to a dynamic import to satisfy ESM publication

--- scripts/generate-rss.ts
+++ scripts/generate-rss.ts
@@ -1,35 +1,39 @@
-import { Feed } from 'feed'
-import fs from 'fs'
-import path from 'path'
-import { getAllPosts } from '../lib/blog'
+const fs = require('fs');
+const path = require('path');
+const { getAllPosts } = require('../lib/blog');
 
-const siteUrl = 'https://orlando-code.github.io/' // <-- change to your real domain
+async function main() {
+  const { Feed } = await import('feed'); // <-- dynamic import
 
-const feed = new Feed({
-  title: "Orlando Timmerman's Blog",
-  description: "Updates and posts",
-  id: siteUrl,
-  link: siteUrl,
-  language: 'en',
-  favicon: `${siteUrl}/favicon.ico`,
-  copyright: `All rights reserved ${(new Date()).getFullYear()}, Orlando Timmerman`,
-  updated: new Date(),
-  generator: 'Next.js + feed',
-})
+  const siteUrl = 'https://orlando-code.github.io/';
 
-const posts = getAllPosts().filter(post => !post.draft)
+  const feed = new Feed({
+    title: "Orlando Timmerman's Blog",
+    description: "Updates and posts",
+    id: siteUrl,
+    link: siteUrl,
+    language: 'en',
+    favicon: `${siteUrl}/favicon.ico`,
+    copyright: `All rights reserved ${(new Date()).getFullYear()}, Orlando Timmerman`,
+    updated: new Date(),
+    generator: 'Next.js + feed',
+  });
 
-posts.forEach(post => {
-  feed.addItem({
-    title: post.title,
-    id: `${siteUrl}/blog/${post.slug}`,
-    link: `${siteUrl}/blog/${post.slug}`,
-    description: post.excerpt || post.description,
-    date: new Date(post.date),
-  })
-})
+  const posts = getAllPosts().filter(post => !post.draft);
 
-// Output RSS and Atom feeds
-const publicDir = path.join(process.cwd(), 'public')
-fs.writeFileSync(path.join(publicDir, 'rss.xml'), feed.rss2())
-fs.writeFileSync(path.join(publicDir, 'atom.xml'), feed.atom1())
+  posts.forEach(post => {
+    feed.addItem({
+      title: post.title,
+      id: `${siteUrl}/blog/${post.slug}`,
+      link: `${siteUrl}/blog/${post.slug}`,
+      description: post.excerpt || '',
+      date: new Date(post.date),
+    });
+  });
+
+  const publicDir = path.join(process.cwd(), 'public');
+  fs.writeFileSync(path.join(publicDir, 'rss.xml'), feed.rss2());
+  fs.writeFileSync(path.join(publicDir, 'atom.xml'), feed.atom1());
+}
+
+main();
